b37c7b4a7619d4d9ccd32e68ec7b8ccda540c196
nhathao04 <nhathao06012004.bl@gmail.com>
2026-07-09 10:21:47 +0700

    feat(quote): manage quote

--- src/modules/quotes/quotes.module.ts
+++ src/modules/quotes/quotes.module.ts
@@ -1,17 +1,25 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { TradeTerm } from "../products/entities/trade-term.entity";
 import { Quote } from "./entities/quote.entity";
 import { QuoteItem } from "./entities/quote-item.entity";
 import { QuoteCertificate } from "./entities/quote-certificate.entity";
-import { QuotesController } from "./quotes.controller";
-import { QuotesService } from "./quotes.service";
 import { User } from "../users/entities/user.entity";
+import { QuotesController } from "./quotes.controller";
+import { TradeTermsController } from "./trade-terms.controller";
+import { QuotesService } from "./quotes.service";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Quote, QuoteItem, QuoteCertificate, User]),
+    TypeOrmModule.forFeature([
+      Quote,
+      QuoteItem,
+      QuoteCertificate,
+      User,
+      TradeTerm,
+    ]),
   ],
-  controllers: [QuotesController],
+  controllers: [QuotesController, TradeTermsController],
   providers: [QuotesService],
   exports: [QuotesService],
 })
